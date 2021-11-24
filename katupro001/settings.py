4b10fda84a1c79eb2c0e65b12d7de6ddda61a6d0
Katuhito Hara <rahakatuhito@gmail.com>
2021-11-24 14:50:59 +0900

    bootstrap_index

--- katupro001/settings.py
+++ katupro001/settings.py
@@ -15,6 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+    
 
 
 # Quick-start development settings - unsuitable for production
@@ -123,8 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    BASE_DIR / 'static',
-    )
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
